02616179e41a8db1958f06ba07700a190075d8ee
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2019-07-24 15:02:08 +0300

    Functional web api sample refactoring

--- samples/MicroServices/NBB.Contracts/NBB.Contracts.Functional.Api/Handlers/Contracts.fs
+++ samples/MicroServices/NBB.Contracts/NBB.Contracts.Functional.Api/Handlers/Contracts.fs
@@ -1,26 +1,20 @@
 ﻿[<AutoOpen>]
 module NBB.Contracts.Handlers.Contracts
 
-open System.Collections.Generic
 open Giraffe
 open NBB.HandlerUtils
 open NBB.Contracts.Application.Commands
 open NBB.Contracts.Application.Queries
-open NBB.Contracts.ReadModel
 
 
 let contractsHandler : HttpHandler =  
     subRoute "/contracts" (
         choose [
-            GET >=> choose [
-                route "" >=> handleQueryFromRequest<GetContracts.Query, List<ContractReadModel>>
-                routeBindNonStrict<GetContractById.Query> "/{id}" handleQuery 
-            ]
-            POST >=> choose [
-                route "" >=> handleCommandFromRequest<CreateContract>
-                routex "/(.*)/lines" >=> handleCommandFromRequest<AddContractLine>
-                routex "/(.*)/validate" >=> handleCommandFromRequest<ValidateContract>
-            ]
+            GET >=> route "" >=> bindQuery<GetContracts.Query> mediatorSendQuery
+            GET >=> route "/(.*)" >=> bindQuery<GetContractById.Query> mediatorSendQuery 
+            POST >=> route "" >=> bindJson<CreateContract> mediatorSendCommand
+            POST >=> routex "/(.*)/lines" >=> bindJson<CreateContract> mediatorSendCommand
+            POST >=> routex "/(.*)/validate" >=> bindJson<CreateContract> mediatorSendCommand
         ])
 
 
--- samples/MicroServices/NBB.Contracts/NBB.Contracts.Functional.Api/Handlers/ContractsAlternative.fs
+++ samples/MicroServices/NBB.Contracts/NBB.Contracts.Functional.Api/Handlers/ContractsAlternative.fs
@@ -6,28 +6,27 @@ open NBB.Contracts.Application.Commands
 open NBB.Contracts.Application.Queries
 open NBB.HandlerUtils
 
-
 let getByIdCustomQuery id =
     let query = GetContractById.Query(Id = id)
-    handleQuery query
+    mediatorSendQuery query
 
 let getByIdOverrideFromRoute id  =
     bindModel<GetContractById.Query> None (fun query ->
         query.Id <- id
-        handleQuery query
+        mediatorSendQuery query
     )
 
 let contractsAlternativeHandler : HttpHandler =  
     subRoute "/contracts2" (
         choose [
             GET >=> choose [
-                route "" >=> bindModel<GetContracts.Query> None handleQuery
+                route "" >=> bindModel<GetContracts.Query> None mediatorSendQuery
                 routef "/%O" getByIdOverrideFromRoute
             ]
             POST >=> choose [
-                route "" >=> bindModel<CreateContract> None handleCommand
-                routex "/(.*)/lines" >=> bindModel<AddContractLine> None handleCommand
-                routex "/(.*)/validate" >=> bindModel<ValidateContract> None handleCommand
+                route "" >=> bindJson<CreateContract> mediatorSendCommand
+                routex "/(.*)/lines" >=> bindJson<AddContractLine> mediatorSendCommand
+                routex "/(.*)/validate" >=> bindJson<ValidateContract> mediatorSendCommand
             ]
         ])
 
