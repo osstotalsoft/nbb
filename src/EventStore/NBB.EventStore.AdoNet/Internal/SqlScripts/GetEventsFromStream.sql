7543ce45de3e42f58183eef3dcee30108df656e8
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2019-11-28 17:12:14 +0200

    Added order to event store select

--- src/EventStore/NBB.EventStore.AdoNet/Internal/SqlScripts/GetEventsFromStream.sql
+++ src/EventStore/NBB.EventStore.AdoNet/Internal/SqlScripts/GetEventsFromStream.sql
@@ -3,3 +3,4 @@
 	WHERE StreamId = @StreamId 
 		AND (@MinStreamVersion IS NULL OR StreamVersion >= @MinStreamVersion)
 		AND (@MaxStreamVersion IS NULL OR StreamVersion <= @MaxStreamVersion)
+	ORDER BY StreamVersion
