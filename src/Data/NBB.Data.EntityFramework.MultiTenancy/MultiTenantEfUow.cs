d4a65254932d9eb0ba07675cc011b1877a001aa4
Dorin Simionescu <dsimionescu@totalsoft.ro>
2019-12-17 10:29:40 +0200

    removed helper

--- src/Data/NBB.Data.EntityFramework.MultiTenancy/MultiTenantEfUow.cs
+++ src/Data/NBB.Data.EntityFramework.MultiTenancy/MultiTenantEfUow.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NBB.Core.Abstractions;
+using NBB.MultiTenancy.Abstractions;
 using NBB.MultiTenancy.Abstractions.Services;
+using NBB.MultiTenancy.Data.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -15,14 +18,14 @@ namespace NBB.Data.EntityFramework.MultiTenancy
     {
         private readonly TContext _c;
         private readonly ILogger<MultiTenantEfUow<TEntity, TContext>> _logger;
-        private readonly MultitenantDbContextHelper _multitenantDbContextHelper;
         private readonly ITenantService _tenantService;
+        private readonly ITenantDatabaseConfigService _tenantDatabaseConfigService;
 
-        public MultiTenantEfUow(TContext c, MultitenantDbContextHelper multitenantDbContextHelper, ITenantService tenantService)
+        public MultiTenantEfUow(TContext c, ITenantService tenantService, ITenantDatabaseConfigService tenantDatabaseConfigService)
         {
             _c = c;
-            _multitenantDbContextHelper = multitenantDbContextHelper;
             _tenantService = tenantService;
+            _tenantDatabaseConfigService = tenantDatabaseConfigService;
         }
 
         public IEnumerable<TEntity> GetChanges()
@@ -33,8 +36,119 @@ namespace NBB.Data.EntityFramework.MultiTenancy
         public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             var tenant = await _tenantService.GetCurrentTenantAsync();
-            _multitenantDbContextHelper.CheckContextIntegrity(_c, tenant);
+            CheckContextIntegrity(_c, tenant);
             await _c.SaveChangesAsync(cancellationToken);            
+        }
+
+        public void UpdateDefaultTenantId(DbContext dbContext, Tenant tenant)
+        {
+            if (tenant == null)
+            {
+                throw new Exception("Tenant could not be identified");
+            }
+
+            var list = dbContext.ChangeTracker.Entries()
+                .Where(e => e.Entity.GetType().GetCustomAttributes(typeof(MustHaveTenantAttribute), true).Length > 0)
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified)
+                .ToList();
+
+            var listByAnnotations =
+                dbContext.ChangeTracker.Entries()
+                .Where(e => e.Metadata.IsMultiTenant() && e.State != EntityState.Unchanged)
+                .ToList();
+
+            list = list.Union(listByAnnotations).ToList();
+
+            foreach (var e in list)
+            {
+                var tenantProp = e.Property("TenantId");
+                if ((Guid)tenantProp.CurrentValue == Guid.Empty)
+                {
+                    tenantProp.CurrentValue = tenant.TenantId;
+                }
+            }
+        }
+
+        protected List<Guid> GetViolations(DbContext dbContext)
+        {
+            var list = GetViolationsByAnnotations(dbContext).Union(GetViolationsByAttributes(dbContext));
+
+            return list.ToList();
+        }
+
+        private List<Guid> GetViolationsByAnnotations(DbContext dbContext)
+        {
+            var list = dbContext
+                        .ChangeTracker
+                        .Entries()
+                        .Where(e => e.Metadata.IsMultiTenant() && e.State != EntityState.Unchanged)
+                        .Select(e => (Guid)dbContext.Entry(e.Entity).Property("TenantId").CurrentValue)
+                        .Distinct()
+                        .ToList();
+
+            return list.Distinct().ToList();
+        }
+
+        private List<Guid> GetViolationsByAttributes(DbContext dbContext)
+        {
+            var list = dbContext
+                          .ChangeTracker
+                          .Entries()
+                          .Where(e => e.Entity.GetType().GetCustomAttributes(typeof(MustHaveTenantAttribute), true).Length > 0 && e.State != EntityState.Unchanged)
+                          .Select(e => (Guid)dbContext.Entry(e.Entity).Property("TenantId").CurrentValue)
+                          .Distinct()
+                          .ToList();
+
+            return list.Distinct().ToList();
+        }
+
+        public void ThrowIfMultipleTenants(DbContext dbContext, Tenant tenant)
+        {
+            if (tenant == null)
+            {
+                throw new Exception("Tenant could not be identified");
+            }
+
+            var toCheck = GetViolations(dbContext);
+
+            if (toCheck.Count == 0)
+            {
+                return;
+            }
+
+            if (!_tenantDatabaseConfigService.IsSharedDatabase(tenant.TenantId))
+            {
+                return;
+            }
+
+            if (toCheck.Count > 1)
+            {
+                throw new CrossTenantUpdateException(toCheck);
+            }
+
+            if (!toCheck.First().Equals(tenant.TenantId))
+            {
+                throw new CrossTenantUpdateException(toCheck);
+            }
+            return;
+        }
+
+        public void CheckContextIntegrity(DbContext dbContext, Tenant tenant)
+        {
+            if (tenant == null)
+            {
+                throw new Exception("Tenant could not be identified");
+            }
+
+            if (_tenantDatabaseConfigService.IsSharedDatabase(tenant.TenantId))
+            {
+                UpdateDefaultTenantId(dbContext, tenant);
+            }
+
+            if (!_tenantDatabaseConfigService.IsSharedDatabase(tenant.TenantId))
+            {
+                ThrowIfMultipleTenants(dbContext, tenant);
+            }
         }
     }
 }
--- src/Data/NBB.Data.EntityFramework.MultiTenancy/MultiTenantEntityTypeConfiguration.cs
+++ src/Data/NBB.Data.EntityFramework.MultiTenancy/MultiTenantEntityTypeConfiguration.cs
@@ -1,13 +1,80 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NBB.MultiTenancy.Abstractions;
+using NBB.MultiTenancy.Abstractions.Services;
+using NBB.MultiTenancy.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace NBB.Data.EntityFramework.MultiTenancy
 {
     public class MultiTenantEntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : class
     {
+        private readonly ITenantService _tenantService;
+        public MultiTenantEntityTypeConfiguration(ITenantService tenantService)
+        {
+            _tenantService = tenantService;
+        }
+
         public void Configure(EntityTypeBuilder<TEntity> builder)
         {
             builder.IsMultiTenant();
+        }
+
+        public void AddQueryFilter<T>(ModelBuilder modelBuilder) where T : class
+        {
+            Expression<Func<T, bool>> filter = t => EF.Property<Guid>(t, "TenantId") == _tenantService.GetCurrentTenantAsync().GetAwaiter().GetResult().TenantId;
+            modelBuilder.Entity<T>().HasQueryFilter((LambdaExpression)filter);
+        }
+
+        public void AddQueryFilters(ModelBuilder modelBuilder, Tenant tenant)
+        {
+            if (tenant == null)
+            {
+                throw new Exception("Tenant could not be identified");
+            }
+
+            var entities = modelBuilder.Model.GetEntityTypes().Where(p => p.ClrType.GetCustomAttributes(typeof(MustHaveTenantAttribute), true).Length > 0).ToList();
+
+            entities.ToList().ForEach(t =>
+            {
+                var method = this.GetType().GetMethod("AddQueryFilter");
+                var genericMethod = method.MakeGenericMethod(t.ClrType);
+                genericMethod.Invoke(this, new object[] { modelBuilder });
+            });
+        }
+
+        private static void AddTenantProperty<T>(ModelBuilder modelBuilder, T entity) where T : class
+        {
+            modelBuilder.Entity<T>()
+                .Property<Guid>("TenantId");
+        }
+
+        public void AddTenantIdProperties(ModelBuilder modelBuilder, Tenant tenant)
+        {
+            if (tenant == null)
+            {
+                throw new Exception("Tenant could not be identified");
+            }
+
+            var mandatory = new List<IMutableEntityType>();
+
+            var entityTypes = modelBuilder.Model.GetEntityTypes();
+            var listByAttributes = modelBuilder.Model.GetEntityTypes().Where(p => p.ClrType.GetCustomAttributes(typeof(MustHaveTenantAttribute), true).Length > 0).ToList();
+
+            var listByAnnotations = modelBuilder.Model.GetEntityTypes().Where(e => e.IsMultiTenant())
+                .Distinct()
+                .ToList();
+
+            var listOfTenantAware = listByAnnotations.Union(listByAttributes).Distinct().ToList();
+            foreach (var entity in listOfTenantAware)
+            {
+                var e = modelBuilder.Entity(entity.ClrType);
+                e.Property(typeof(Guid), "TenantId");
+            }
         }
     }
 }
