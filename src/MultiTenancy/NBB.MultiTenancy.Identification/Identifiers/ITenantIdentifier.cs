cbc8cf03fc840b26e4428b3d88fb9b28680cb82c
Dragos Rosca <drosca@totalsoft.ro>
2020-01-06 16:19:53 +0200

    Added tenant identification and a test

---
+++ src/MultiTenancy/NBB.MultiTenancy.Identification/Identifiers/ITenantIdentifier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NBB.MultiTenancy.Identification.Identifiers
+{
+    public interface ITenantIdentifier
+    {
+        Task<Guid> GetTenantIdAsync(string tenantToken);
+    }
+}
---
+++ src/MultiTenancy/NBB.MultiTenancy.Identification/Services/AbstractTenantService.cs
@@ -0,0 +1,25 @@
+﻿using NBB.MultiTenancy.Abstractions.Services;
+using System;
+using System.Threading.Tasks;
+using NBB.MultiTenancy.Identification.Identifiers;
+
+namespace NBB.MultiTenancy.Identification.Services
+{
+    public abstract class AbstractTenantService : ITenantService
+    {
+        protected ITenantIdentifier Identifier;
+
+        protected AbstractTenantService(ITenantIdentifier identifier)
+        {
+            Identifier = identifier;
+        }
+
+        public virtual async Task<Guid> GetTenantIdAsync()
+        {
+            var token = await GetTenantToken();
+            return await Identifier.GetTenantIdAsync(token);
+        }
+
+        public abstract Task<string> GetTenantToken();
+    }
+}
