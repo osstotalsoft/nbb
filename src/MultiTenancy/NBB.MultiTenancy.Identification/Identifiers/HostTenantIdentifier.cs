1652412c397abc54f371860527efc4aaa15e61e6
Dragos Rosca <drosca@totalsoft.ro>
2020-01-07 14:49:35 +0200

    added a host tenant identifier with tests and repository

---
+++ src/MultiTenancy/NBB.MultiTenancy.Identification/Identifiers/HostTenantIdentifier.cs
@@ -0,0 +1,21 @@
+﻿using NBB.MultiTenancy.Identification.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace NBB.MultiTenancy.Identification.Identifiers
+{
+    public class HostTenantIdentifier : ITenantIdentifier
+    {
+        private readonly IHostTenantRepository _hostTenantRepository;
+
+        public HostTenantIdentifier(IHostTenantRepository hostTenantRepository)
+        {
+            _hostTenantRepository = hostTenantRepository;
+        }
+
+        public Task<Guid> GetTenantIdAsync(string tenantToken)
+        {
+            return _hostTenantRepository.GetTenantId(tenantToken);
+        }
+    }
+}
---
+++ test/UnitTests/MultiTenancy/NBB.MultiTenancy.Identification.Tests/Identifiers/HostTenantIdentifierTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Moq;
+using NBB.MultiTenancy.Identification.Identifiers;
+using NBB.MultiTenancy.Identification.Repositories;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NBB.MultiTenancy.Identification.Tests.Identifiers
+{
+    public class HostTenantIdentifierTests
+    {
+        private readonly Mock<IHostTenantRepository> _hostTenantRepository;
+
+        public HostTenantIdentifierTests()
+        {
+            _hostTenantRepository = new Mock<IHostTenantRepository>();
+        }
+
+        [Fact]
+        public void Should_Return_TenantId()
+        {
+            // Arrange
+            var tenantId = Guid.NewGuid();
+            _hostTenantRepository.Setup(r => r.GetTenantId(It.IsAny<string>())).Returns(Task.FromResult(tenantId));
+            var sut = new HostTenantIdentifier(_hostTenantRepository.Object);
+
+            // Act
+            var result = sut.GetTenantIdAsync(string.Empty).Result;
+
+            // Assert
+            result.Should().Be(tenantId);
+        }
+
+        [Fact]
+        public void Should_Pass_Token_To_Repository()
+        {
+            // Arrange
+            const string tenantToken = "tenant token";
+            var sut = new HostTenantIdentifier(_hostTenantRepository.Object);
+
+            // Act
+            var result = sut.GetTenantIdAsync(tenantToken).Result;
+
+            // Assert
+            _hostTenantRepository.Verify(r => r.GetTenantId(It.Is<string>(s => string.Equals(s, tenantToken))), Times.Once());
+        }
+    }
+}
