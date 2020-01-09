496a828e71af793827258018ceeeed822729d481
Radu Popovici <oncicaradupopovici@gmail.com>
2020-01-09 17:05:20 +0200

    FSharp effects (#52)

---
+++ samples/MicroServices/NBB.Contracts/NBB.Contracts.FSharp/Library.fs
@@ -0,0 +1,59 @@
+﻿namespace NBB.Contracts.FSharp
+
+open NBB.Core.Effects.FSharp
+
+module Domain =
+    module ContractAggregate =
+        type ContractStatus = Draft | Validated
+
+        type Contract = {
+            ContractId: int
+            Value: decimal
+            Status: ContractStatus
+        }
+
+        type DomainEvent = 
+            | ContractCreated of Contract
+            | ContractValidated of Contract
+
+        let create contractId value = { ContractId = contractId; Value = value; Status = Draft }
+        let validate contract = { contract with Status = Validated }
+
+
+module Data =
+    module Contract =
+        open Domain.ContractAggregate
+        let loadById contractId = Effect.pureEffect { ContractId = contractId; Value = 78m; Status = Draft; }
+        let save (contract:Contract) = Effect.pureEffect contract |> Effect.ignore
+
+
+module Application =
+    open Domain.ContractAggregate
+    open Data.Contract
+    open NBB.Messaging.Effects
+
+    type Event =
+        | ContractCreated of Contract
+        | ContractValidated of Contract
+
+    type Command =
+        | CreteContract of int * decimal
+        | ValidateContract of int
+
+
+    let handle cmd = 
+        match cmd with
+        | CreteContract (contractId, value) ->
+            effect {
+                let contract = create contractId value
+                do! save contract
+                do! MessageBus.Publish (ContractCreated contract) |> Effect.ignore
+            }
+
+        | ValidateContract contractId ->
+            effect {
+                let! contract = loadById contractId
+                let contract' = contract |> validate
+                do! contract' |> save
+                do! MessageBus.Publish (ContractValidated contract) |> Effect.ignore
+            }
