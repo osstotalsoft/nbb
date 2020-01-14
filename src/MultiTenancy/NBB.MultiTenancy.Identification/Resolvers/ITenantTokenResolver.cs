0c03e8711b1f1a81297b52d2091323c33a925643
Dragos Rosca <drosca@totalsoft.ro>
2020-01-14 15:17:49 +0200

    Documented the interface

--- src/MultiTenancy/NBB.MultiTenancy.Identification/Resolvers/ITenantTokenResolver.cs
+++ src/MultiTenancy/NBB.MultiTenancy.Identification/Resolvers/ITenantTokenResolver.cs
@@ -4,6 +4,11 @@ namespace NBB.MultiTenancy.Identification.Resolvers
 {
     public interface ITenantTokenResolver
     {
+        /// <summary>
+        /// Gets tenant identification token form a context
+        /// </summary>
+        /// <returns>Tenant token</returns>
+        /// <exception cref="CannotResolveTokenException"></exception>
         Task<string> GetTenantToken();
     }
 }
