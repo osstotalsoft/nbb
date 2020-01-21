2e910595af5f8d5118971c95d46f114464c968bd
dragos rosca <26022019+dragos-rosca@users.noreply.github.com>
2020-01-21 15:20:47 +0200

    Added resolver to get host from requester. (#57)
    * Added resolver to get host from requester.
    
    * Renamed the resolver

---
+++ src/MultiTenancy/NBB.MultiTenancy.Identification.Http/HostRefererHttpTokenResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using NBB.MultiTenancy.Identification.Resolvers;
+using System.Threading.Tasks;
+
+namespace NBB.MultiTenancy.Identification.Http
+{
+    public class HostRefererHttpTokenResolver : ITenantTokenResolver
+    {
+        private const string HeaderReferer = "Referer";
+        private readonly HttpContext _httpContext;
+
+        public HostRefererHttpTokenResolver(IHttpContextAccessor accessor)
+        {
+            _httpContext = accessor.HttpContext;
+        }
+
+        public Task<string> GetTenantToken()
+        {
+            var headers = _httpContext?.Request?.Headers;
+            if (headers == null || !headers.TryGetValue(HeaderReferer, out var headerReferer))
+            {
+                return Task.FromResult<string>(null);
+            }
+
+            var host = new Uri(headerReferer.ToString()).DnsSafeHost;
+            return Task.FromResult(host);
+
+        }
+    }
+}
