44ea5d4d92f2dddb3df4c476cbcc2e4698393f9b
Radu Popovici <rpopovici@totalsoft.ro>
2020-02-14 10:18:21 +0200

    list sequence effects and list sequence evented

--- src/Core/NBB.Core.Effects.FSharp/Effects.fs
+++ src/Core/NBB.Core.Effects.FSharp/Effects.fs
@@ -4,21 +4,21 @@ open System
 open NBB.Core.Effects
 
 module Effect = 
-    let bind eff func = Effect.Bind(eff, Func<'a, IEffect<'b>>(func))
+    let bind func eff = Effect.Bind(eff, Func<'a, IEffect<'b>>(func))
     let map func eff = Effect.Map(Func<'a, 'b>(func), eff)
-    let apply func eff = Effect.Apply(func, eff)
+    let apply (func: IEffect<'a->'b>) eff = Effect.Apply(map (fun fn -> Func<'a,'b>(fn)) func, eff)
     let pureEffect x = Effect.Pure x
     let ignore eff = map (fun _ -> ()) eff
 
-    let composeK f g x = bind (f x) g
+    let composeK f g x = bind g (f x)
     let lift2 f = map f >> apply
 
 module EffectBuilder =
     type EffectBuilder() =
-        member _.Bind(eff, func) = Effect.bind eff func
+        member _.Bind(eff, func) = Effect.bind func eff
         member _.Return(value) = Effect.pureEffect value
         member _.ReturnFrom(value) = value
-        member _.Combine(eff1, eff2) = Effect.bind eff1 (fun _ -> eff2)
+        member _.Combine(eff1, eff2) = Effect.bind (fun _ -> eff2) eff1
         member _.Zero() = Effect.pureEffect ()
 
 
@@ -28,8 +28,17 @@ module Effects =
 
     let (<!>) = Effect.map
     let (<*>) = Effect.apply
-    let (>>=) = Effect.bind
+    let (>>=) eff func = Effect.bind func eff
     let (>=>) = Effect.composeK
+
+
+module List =
+    let sequenceEffect list =
+        let cons head tail = head :: tail      
+        let initState = Effect.pureEffect []
+        let folder head tail = Effect.pureEffect cons <*> head <*> tail
+
+        List.foldBack folder list initState
 
 module private Tests =
     module Domain =
@@ -54,3 +63,7 @@ module private Tests =
             }
 
         let handler' (IncrementCommand id) = id |> loadById |> Effect.map increment >>= save
+
+        let handler'' (IncrementCommand id) = 
+            let handle = loadById >> Effect.map increment >> Effect.bind save
+            handle id
--- src/Core/NBB.Core.Evented.FSharp/Evented.fs
+++ src/Core/NBB.Core.Evented.FSharp/Evented.fs
@@ -5,7 +5,7 @@ type Evented<'a, 'e> = Evented of payload:'a * events:'e list
 module Evented = 
     let map func (Evented(value, events)) = Evented(func value, events)
 
-    let bind (Evented(value, events)) func = 
+    let bind func (Evented(value, events)) = 
         let (Evented(result, events')) = func value
         Evented(result, events @ events')
 
@@ -13,16 +13,16 @@ module Evented =
 
     let result value = Evented(value, [])
 
-    let composeK f g x = bind (f x) g
+    let composeK f g x = bind g (f x)
 
     let lift2 f = map f >> apply
 
 module EventedBuilder =
     type EventedBuilder() =
-        member _.Bind(eff, func) = Evented.bind eff func
+        member _.Bind(evented, func) = Evented.bind func evented
         member _.Return(value) = Evented.result value
         member _.ReturnFrom(value) = value
-        member _.Combine(eff1, eff2) = Evented.bind eff1 (fun _ -> eff2)
+        member _.Combine(evented1, evented2) = Evented.bind (fun _ -> evented2) evented1
         member _.Zero() = Evented.result ()
 
 [<AutoOpen>]
@@ -31,8 +31,16 @@ module Events =
 
     let (<!>) = Evented.map
     let (<*>) = Evented.apply
-    let (>>=) = Evented.bind
+    let (>>=) evented func = Evented.bind func evented
     let (>=>) = Evented.composeK
+
+module List =
+    let sequenceEvented list =
+        let cons head tail = head :: tail  
+        let initState = Evented.result []
+        let folder head tail = Evented.result cons <*> head <*> tail
+
+        List.foldBack folder list initState
 
 
 module private Tests =
@@ -47,15 +55,16 @@ module private Tests =
     let increment (AggRoot x) = AggRoot (x + 1)
 
     let createAndUpdate x = x |> create >>= update
-    let createAndUpdate''' = create >=> update
-    let createAndUpdate'''' x =
+    let createAndUpdate' = create >> Evented.bind update
+    let createAndUpdate'' = create >=> update
+    let createAndUpdate''' x =
         evented {
             let! x' = create x
             let! x'' = update x'
             return x''
         }
 
-    let createAndUpdate'''''  x =
+    let createAndUpdate''''  x =
         let (Evented(agg, events)) = create x
         let (Evented(agg', events')) = update agg
         Evented(agg', events @ events')
