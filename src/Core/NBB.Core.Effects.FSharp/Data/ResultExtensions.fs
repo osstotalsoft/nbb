43a841f9dec2d09485834dcaa9112ce08beaf9b1
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2020-03-10 12:54:14 +0200

    Added F#  data structures like (Reader and State monads) as well as Traversable implementations

---
+++ src/Core/NBB.Core.Effects.FSharp/Data/ResultExtensions.fs
@@ -0,0 +1,16 @@
+﻿namespace NBB.Core.Effects.FSharp.Data
+
+[<AutoOpen>]
+module ResultExtensions =
+
+    [<RequireQualifiedAccess>]
+    module List =
+        let traverseResult f list =
+            let pure' = Result.Ok
+            let (<*>) fn = Result.bind (fun x-> Result.map (fun f -> f x) fn) 
+            let cons head tail = head :: tail  
+            let initState = pure' []
+            let folder head tail = pure' cons <*> (f head) <*> tail
+            List.foldBack folder list initState
+
+        let sequenceResult list = traverseResult id list
