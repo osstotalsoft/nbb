cc675e327c7fead2cf0f2faf5e687ced209df7c1
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2020-08-05 13:17:28 +0300

    Added option to register functions as side effect hanlers

---
+++ src/Core/NBB.Core.Effects/GenericSideEffectHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NBB.Core.Effects
+{
+    public class GenericSideEffectHandler<TSideEffect, TOutput>: ISideEffectHandler<TSideEffect, TOutput>
+        where TSideEffect : ISideEffect<TOutput>
+    {
+        private readonly Func<TSideEffect, CancellationToken, Task<TOutput>> handlerFn;
+
+        public GenericSideEffectHandler(Func<TSideEffect, CancellationToken, Task<TOutput>> handlerFn)
+        {
+            this.handlerFn = handlerFn;
+        }
+
+        public Task<TOutput> Handle(TSideEffect sideEffect, CancellationToken cancellationToken = default)
+        {
+            return handlerFn(sideEffect, cancellationToken);
+        }
+    }
+}
