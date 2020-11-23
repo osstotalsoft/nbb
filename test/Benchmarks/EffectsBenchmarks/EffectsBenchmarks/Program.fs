c210849301404299987ed557d22f6c5008e6ac86
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2020-11-23 16:01:56 +0200

    Feature/effects performance optimization (#98)
    * Split effect computation effects into strict and lazy
    
    * Added benchmark for lazy / strict effect CE comparison

---
+++ test/Benchmarks/EffectsBenchmarks/EffectsBenchmarks/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open BenchmarkDotNet.Running
+
+[<EntryPoint>]
+let main argv =
+    BenchmarkRunner.Run<EffectBenchmark.Benchmark>() |> ignore
+    0 // return an integer exit code
