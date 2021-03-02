1b02f5fa2701d29d3e1dd6f09c9534d99d6b5b78
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2021-03-02 12:53:18 +0200

    Added data contracts and legacy topic registry decorator

---
+++ src/Messaging/NBB.Messaging.BackwardCompatibility/DependencyInjectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NBB.Messaging.Abstractions;
+
+namespace NBB.Messaging.BackwardCompatibility
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection UseLegacyTopicRegistry(this IServiceCollection services)
+        {
+            services.Decorate<ITopicRegistry, LegacyTopicRegistryDecorator>();
+            return services;
+        }
+    }
+}
