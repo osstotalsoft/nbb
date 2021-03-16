d9ae96ca2f1da566b327805dcb568ae5d7687617
dorinsimionescu <38032608+dorinsimionescu@users.noreply.github.com>
2021-03-16 09:51:16 +0200

    added sample json (#122)
    Co-authored-by: dsimionescu@totalsoft.ro <dsimionescu@totalsoft.ro>

---
+++ src/Application/NBB.Application.DataContracts.Schema/Sample/SampleBuilder.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using System;
+
+namespace NBB.Application.DataContracts.Schema.Sample
+{
+    public class SampleBuilder
+    {
+        public T GetSample<T>() where T : class
+        {
+            var fixture = new Fixture();
+            fixture.Customizations.Add(new StringSpecimenBuilder());
+
+            var sample = fixture.Create<T>();
+            return sample;
+
+        }
+
+        public object GetSampleFromType(Type type)
+        {
+            var method = typeof(SampleBuilder).GetMethod("GetSample");
+
+            var genericMethod = method.MakeGenericMethod(type);
+            var obj = genericMethod.Invoke(this, null); 
+            return obj;
+        }
+    }
+}
--- src/Application/NBB.Application.DataContracts.Schema/SchemaDefinition.cs
+++ src/Application/NBB.Application.DataContracts.Schema/SchemaDefinition.cs
@@ -6,13 +6,15 @@
         public string FullName { get; set; }
         public string Schema { get; set; }
         public string Topic { get; set; }
+        public string SampleJson { get; set; }
 
-        public SchemaDefinition(string name, string fullName, string schema, string topic)
+        public SchemaDefinition(string name, string fullName, string schema, string topic, string sampleJson)
         {
             Name = name;
             FullName = fullName;
             Schema = schema;
             Topic = topic;
+            SampleJson = sampleJson;
         }
     }
 }
