20dfc811a4d1047214398dd426f4dd704cd69836
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2021-05-06 19:00:30 +0300

    Implemented NBB.Core.Effects.Unit equality (#138)

--- src/Core/NBB.Core.Effects/Unit.cs
+++ src/Core/NBB.Core.Effects/Unit.cs
@@ -1,10 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace NBB.Core.Effects
 {
-    public struct Unit
+    public struct Unit : IEquatable<Unit>
     {
         public static readonly Unit Value = new Unit();
         public static readonly Task<Unit> Task = System.Threading.Tasks.Task.FromResult(Value);
+
+        public override int GetHashCode() => 0;
+        public bool Equals(Unit other) => true;
+        public override bool Equals(object obj) => obj is Unit;
+        public static bool operator ==(Unit _first, Unit _second) => true;
+        public static bool operator !=(Unit _first, Unit _second) => false;
     }
 }
