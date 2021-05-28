5399009d6e25adcd046511b79b448e07e6729da3
Radu Popovici <rpopovici@totalsoft.ro>
2021-05-28 14:21:15 +0300

    rest of commit

---
+++ samples/MicroServices/NBB.Invoices.FSharp/NBB.Invoices.FSharp.Worker/Program.fs
@@ -0,0 +1,79 @@
+﻿open System
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Configuration
+open System.Reflection
+open System.IO
+open Microsoft.Extensions.DependencyInjection
+open NBB.Application.Mediator.FSharp
+open NBB.Invoices.FSharp.Application
+open NBB.Core.Effects
+open NBB.Messaging.Effects
+open NBB.Messaging.Abstractions
+open NBB.Messaging.Host
+open NBB.Messaging.Nats
+open Microsoft.Extensions.Logging
+open NBB.Messaging.Host.MessagingPipeline
+
+[<EntryPoint>]
+let main argv =
+
+    // App configuration
+    let appConfig (context: HostBuilderContext) (configApp: IConfigurationBuilder) =
+        configApp
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional = true)
+            .AddJsonFile(sprintf "appsettings.%s.json" context.HostingEnvironment.EnvironmentName, optional = true)
+            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .AddEnvironmentVariables()
+            .AddCommandLine(argv)
+        |> ignore
+
+    // Services configuration
+    let serviceConfig (context: HostBuilderContext) (services: IServiceCollection) =
+        services |> WriteApplication.addServices
+
+        services
+            .AddMessageBus()
+            .AddNatsTransport(context.Configuration)
+        |> ignore
+
+        services.AddMessagingHost
+            (fun hostBuilder ->
+                hostBuilder.Configure
+                    (fun configBuilder ->
+                        configBuilder
+                            .AddSubscriberServices(fun config ->
+                                config.AddTypes(typeof<InvoiceApplication.Command>)
+                                |> ignore)
+                            .WithDefaultOptions()
+                            .UsePipeline(fun pipelineBuilder ->
+                                pipelineBuilder
+                                    .UseCorrelationMiddleware()
+                                    .UseExceptionHandlingMiddleware()
+                                    .UseDefaultResiliencyMiddleware()
+                                    .UseEffectMiddleware(fun m ->
+                                        m
+                                        |> Mediator.sendMessage
+                                        |> EffectExtensions.ToUnit)
+                                |> ignore)
+                        |> ignore)
+                |> ignore)
+        |> ignore
+
+    // Logging configuration
+    let loggingConfig (context: HostBuilderContext) (loggingBuilder: ILoggingBuilder) =
+        loggingBuilder.AddConsole().AddDebug() |> ignore
+
+    let host =
+        HostBuilder()
+            .ConfigureAppConfiguration(Action<HostBuilderContext, IConfigurationBuilder> appConfig)
+            .ConfigureServices(Action<HostBuilderContext, IServiceCollection> serviceConfig)
+            .ConfigureLogging(Action<HostBuilderContext, ILoggingBuilder> loggingConfig)
+            .UseConsoleLifetime()
+            .Build()
+
+    host.RunAsync()
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
+    0
---
+++ samples/MicroServices/NBB.Invoices.FSharp/NBB.Invoices.FSharp/Application/MessageBus.fs
@@ -0,0 +1,10 @@
+﻿namespace NBB.Invoices.FSharp.Application
+
+open NBB.Core.Effects.FSharp
+
+// TODO: Find a place for MesageBus wrapper
+module MessageBus =
+    let publish (obj: 'TMessage) =
+        NBB.Messaging.Effects.MessageBus.Publish(obj :> obj)
+        |> Effect.ignore
+
