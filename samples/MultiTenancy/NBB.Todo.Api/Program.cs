8a17271266e11e4582c48efd1f477952e8e529de
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2021-05-28 21:57:07 +0300

    logging fix

--- samples/MultiTenancy/NBB.Todo.Api/Program.cs
+++ samples/MultiTenancy/NBB.Todo.Api/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace NBB.Todo.Api
 {
@@ -7,14 +10,29 @@ namespace NBB.Todo.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            Log.Information("Starting NBB.Todo.Api");
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureLogging(ConfigureLogging)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void ConfigureLogging(HostBuilderContext ctx, ILoggingBuilder _builder)
+        {
+            Log.Logger = new LoggerConfiguration()
+              .ReadFrom.Configuration(ctx.Configuration)
+              .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+              .Enrich.FromLogContext()
+              .WriteTo.Console()
+              .CreateLogger();
+        }
     }
 }
--- samples/MultiTenancy/NBB.Todo.Worker/Program.cs
+++ samples/MultiTenancy/NBB.Todo.Worker/Program.cs
@@ -1,26 +1,40 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using NBB.Todos.Data;
+using NBB.Messaging.Abstractions;
+using NBB.Messaging.Nats;
+using NBB.Todo.Worker.Application;
+using NBB.Messaging.Host;
+using NBB.MultiTenancy.Abstractions.Options;
+using Microsoft.Extensions.Options;
+using NBB.Messaging.Host.Builder;
+using NBB.Messaging.Host.MessagingPipeline;
+using NBB.MultiTenancy.Abstractions.Hosting;
+using NBB.Messaging.MultiTenancy;
+using NBB.MultiTenancy.Abstractions.Repositories;
+using NBB.MultiTenancy.Identification.Messaging.Extensions;
+using Serilog.Events;
+using Microsoft.Extensions.Logging;
 
 namespace NBB.Todo.Worker
 {
     public class Program
     {
-        public static IConfiguration Configuration { get; private set; }
-
         public static async Task<int> Main(string[] args)
         {
             try
             {
-                var host = BuildWebHost(args);
+                var host = BuildConsoleHost(args);
+
                 Log.Information("Starting NBB.Tasks.Worker");
-                Log.Information("Messaging.Env=" + Configuration.GetSection("Messaging")["Env"]);
 
                 await host.RunAsync(CancellationToken.None);
 
@@ -37,24 +51,84 @@ namespace NBB.Todo.Worker
             }
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(cb => BuildConfiguration(cb))
-                .UseStartup<Startup>()
-                //.UseSerilog()
+        public static IHost BuildConsoleHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(config => config.AddEnvironmentVariables("NETCORE_"))
+                .ConfigureLogging(ConfigureLogging)
+                .ConfigureAppConfiguration(ConfigureApp)
+                .ConfigureServices(ConfigureServices)
+                .UseSerilog()
+                .UseConsoleLifetime()
                 .Build();
 
-        private static void BuildConfiguration(IConfigurationBuilder configurationBuilder)
+        private static void ConfigureServices(HostBuilderContext hostingContext, IServiceCollection services)
         {
-            Configuration = configurationBuilder
+            // MediatR 
+            services.AddMediatR(typeof(CreateTodoTaskHandler).Assembly);
+
+            // Data
+            services.AddTodoDataAccess();
+
+            // Messaging
+            services.AddMessageBus().AddNatsTransport(hostingContext.Configuration);
+
+            services.AddMessagingHost(hostBuilder => hostBuilder
+                .Configure(configBuilder =>
+                {
+                    var tenancyOptions = configBuilder.ApplicationServices.GetRequiredService<IOptions<TenancyHostingOptions>>();
+                    var isMultiTenant = tenancyOptions?.Value?.TenancyType != TenancyType.None;
+
+                    configBuilder
+                        .AddSubscriberServices(selector => selector.
+                            FromMediatRHandledCommands().AddAllClasses())
+                        .WithDefaultOptions()
+                        .UsePipeline(builder => builder
+                            .UseCorrelationMiddleware()
+                            .UseExceptionHandlingMiddleware()
+                            .When(isMultiTenant, x => x.UseTenantMiddleware())
+                            .UseDefaultResiliencyMiddleware()
+                            .UseMediatRMiddleware()
+                    );
+                }));
+
+            Log.Information("Messaging.Env=" + hostingContext.Configuration.GetSection("Messaging")["Env"]);
+
+            // Multitenancy
+            services.AddMultitenancy(hostingContext.Configuration, _ =>
+            {
+                services.AddMultiTenantMessaging()
+                        .AddDefaultMessagingTenantIdentification()
+                        .AddTenantRepository<BasicTenantRepository>();
+
+                services.AddMultiTenantTodoDataAccess();
+            });
+        }
+
+        private static void ConfigureApp(HostBuilderContext ctx, IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile(
-                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                    $"appsettings.{Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT") ?? "Production"}.json",
                     optional: true, reloadOnChange: true)
-                .AddUserSecrets<Program>()
-                .AddEnvironmentVariables()
-                .Build();
+                .AddEnvironmentVariables();
+
+
+            if (ctx.HostingEnvironment.IsDevelopment())
+            {
+                configurationBuilder.AddUserSecrets<Program>();
+            }
+        }
+
+        private static void ConfigureLogging(HostBuilderContext ctx, ILoggingBuilder _builder)
+        {
+            Log.Logger = new LoggerConfiguration()
+              .ReadFrom.Configuration(ctx.Configuration)
+              .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+              .Enrich.FromLogContext()
+              .WriteTo.Console()
+              .CreateLogger();          
         }
     }
 }
