cf65207588ea1708fa2272bc3a16f3c733ff1f7a
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2021-06-04 22:22:55 +0300

    Allow tenant EF global query filter  to be parameterized by dynamic tenant id.

--- src/Data/NBB.Data.EntityFramework.MultiTenancy/MultiTenantEntityTypeConfigurationDecorator.cs
+++ src/Data/NBB.Data.EntityFramework.MultiTenancy/MultiTenantEntityTypeConfigurationDecorator.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using NBB.MultiTenancy.Abstractions.Context;
 using NBB.MultiTenancy.Abstractions.Options;
 
 namespace NBB.Data.EntityFramework.MultiTenancy
@@ -13,14 +11,15 @@ namespace NBB.Data.EntityFramework.MultiTenancy
     {
         private readonly IEntityTypeConfiguration<TEntity> _innerConfiguration;
         private readonly IServiceProvider _sp;
+        private readonly DbContext _dbContext;
         private readonly bool _isMultiTenant;
         private string _tenantIdColumn;
 
-        public MultiTenantEntityTypeConfigurationDecorator(IEntityTypeConfiguration<TEntity> innerConfiguration, IServiceProvider sp)
+        public MultiTenantEntityTypeConfigurationDecorator(IEntityTypeConfiguration<TEntity> innerConfiguration, IServiceProvider sp, DbContext dbContext)
         {
             _innerConfiguration = innerConfiguration;
             _sp = sp;
-
+            _dbContext = dbContext;
             var tenancyOptions = _sp.GetRequiredService<IOptions<TenancyHostingOptions>>();
             _isMultiTenant = tenancyOptions?.Value?.TenancyType != TenancyType.None;
         }
@@ -38,38 +37,31 @@ namespace NBB.Data.EntityFramework.MultiTenancy
             if (!_isMultiTenant)
                 return;
 
-            var tenantId = ResolveTenantId();
-            var isSharedDb = IsSharedDb(tenantId);
+            var isSharedDb = IsSharedDb();
 
             builder.HasAnnotation(MultiTenancy.MultiTenantAnnotation, true);
             AddTenantIdProperty(builder);
-            AddTenantIdQueryFilter(builder, tenantId, isSharedDb);
+            AddTenantIdQueryFilter(builder, isSharedDb);
         }
 
-        private Guid ResolveTenantId()
+        private bool IsSharedDb()
         {
-            var tenantContextAccessor = _sp.GetRequiredService<ITenantContextAccessor>();
-            var tenantId = tenantContextAccessor.TenantContext.GetTenantId();
+            var tenantId = _dbContext.GetTenantIdFromContext();
 
-            return tenantId;
-        }
-
-        private bool IsSharedDb(Guid tenantId)
-        {
             var tenantDatabaseConfigService = _sp.GetRequiredService<ITenantDatabaseConfigService>();
             return tenantDatabaseConfigService.IsSharedDatabase(tenantId);
         }
 
-        private void AddTenantIdQueryFilter(EntityTypeBuilder<TEntity> builder, Guid tenantId, bool isSharedDb)
+        private void AddTenantIdQueryFilter(EntityTypeBuilder<TEntity> builder, bool isSharedDb)
         {
             if (!isSharedDb)
             {
                 return;
             }
 
-            Expression<Func<TEntity, bool>> filter = t =>
-                EF.Property<Guid>(t, MultiTenancy.TenantIdProp) == tenantId;
-            builder.HasQueryFilter(filter);
+            https://github.com/dotnet/efcore/pull/11017
+            builder.HasQueryFilter(t =>
+                EF.Property<Guid>(t, MultiTenancy.TenantIdProp) == _dbContext.GetTenantIdFromContext());
         }
 
         private void AddTenantIdProperty(EntityTypeBuilder<TEntity> builder)
