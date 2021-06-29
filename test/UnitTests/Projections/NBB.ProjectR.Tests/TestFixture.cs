b0fb962c8f2f0e8fbbdcadc876bfbb73dac55ccd
Radu Popovici <rpopovici@totalsoft.ro>
2021-06-29 09:35:21 +0300

    yet another one

---
+++ test/UnitTests/Projections/NBB.ProjectR.Tests/TestFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NBB.Application.MediatR.Effects;
+using NBB.Core.Effects;
+using NBB.Messaging.Abstractions;
+using NBB.Messaging.Effects;
+using NBB.Messaging.InProcessMessaging.Extensions;
+
+namespace NBB.ProjectR.Tests
+{
+    public class TestFixture
+    {
+        public ServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddProjectR(GetType().Assembly);
+            services.AddMediatR(GetType().Assembly);
+            services
+                .AddEffects()
+                .AddMessagingEffects()
+                .AddMediatorEffects();
+            services.AddMessageBus().AddInProcessTransport();
+            services.AddLogging();
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+
+            return services.BuildServiceProvider();
+
+        }
+    }
+}
--- test/UnitTests/Projections/NBB.ProjectR.Tests/UnitTest1.cs
+++ test/UnitTests/Projections/NBB.ProjectR.Tests/UnitTest1.cs
@@ -1,27 +1,34 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentAssertions;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NBB.Application.MediatR.Effects;
 using NBB.Core.Effects;
+using NBB.Messaging.Abstractions;
+using NBB.Messaging.Effects;
+using NBB.Messaging.InProcessMessaging.Extensions;
 using Xunit;
 using Unit = NBB.Core.Effects.Unit;
-using NBB.Messaging.Effects;
 using Mediator = NBB.Application.MediatR.Effects.Mediator;
+using MessageBus = NBB.Messaging.Effects.MessageBus;
 
 namespace NBB.ProjectR.Tests
 {
-    public class UnitTest1
+    public class UnitTest1 : IClassFixture<TestFixture>
     {
         record ContractCreated(Guid ContractId, decimal Value) : INotification;
         record ContractValidated(Guid ContractId, Guid UserId) : INotification;
 
 
-        class ContractProjection
+        public class ContractProjection
         {
-            record Projection(Guid ContractId, bool IsValidated, Guid? ValidatedByUserId, string ValidatedByUsername) : IHaveIdentityOf<Guid>;
+            public record Projection(Guid ContractId, bool IsValidated, Guid? ValidatedByUserId, string ValidatedByUsername) : IHaveIdentityOf<Guid>;
 
-            record UserLoaded(Guid ContractId, Guid UserId, string Username) : INotification;
+            public record UserLoaded(Guid ContractId, Guid UserId, string Username) : INotification;
 
             class Projector :
                 IProjector<ContractCreated, Projection, Guid>,
@@ -63,23 +70,41 @@ namespace NBB.ProjectR.Tests
 
         }
 
+        private readonly TestFixture _fixture;
+
+        public UnitTest1(TestFixture fixture)
+        {
+            _fixture = fixture;
+        }
 
         [Fact]
         public async Task Test1()
         {
             //Arrange
-            var services = new ServiceCollection();
-            services.AddProjectR(GetType().Assembly);
-            services.AddMediatR(GetType().Assembly);
-            await using var container = services.BuildServiceProvider();
+            await using var container = _fixture.BuildServiceProvider();
             using var scope = container.CreateScope();
             var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+            var repo = scope.ServiceProvider
+                .GetRequiredService<IProjectionStore<ContractProjection.Projection, Guid>>();
+            var messageBus = scope.ServiceProvider.GetRequiredService<IMessageBus>();
+            using var _ = await messageBus.SubscribeAsync<ContractProjection.UserLoaded>(msg =>
+                mediator.Publish(msg.Payload, default)
+            );
+
+
             var contractId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+
+            
 
             //Act
             await mediator.Publish(new ContractCreated(contractId, 100));
+            await mediator.Publish(new ContractValidated(contractId, userId));
+            
 
             //Assert
+            var projection = await repo.LoadById(contractId, CancellationToken.None);
+            projection.Should().NotBeNull();
         }
     }
 
