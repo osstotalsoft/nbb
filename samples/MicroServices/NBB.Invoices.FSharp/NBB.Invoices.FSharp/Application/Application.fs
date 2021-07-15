608cc2ac0aad6074cc5e399b860a187ad680ce0d
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2021-07-15 10:27:40 +0300

    PipelineUtils and postman collection update

--- samples/MicroServices/NBB.Invoices.FSharp/NBB.Invoices.FSharp/Application/Application.fs
+++ samples/MicroServices/NBB.Invoices.FSharp/NBB.Invoices.FSharp/Application/Application.fs
@@ -26,14 +26,7 @@ module Middlewares =
                 return Some()
             }
 
-module PipelineUtils =
-    let terminateRequest<'a> : (Effect<'a option> -> Effect<'a>) =
-        Effect.map
-            (function
-            | Some value -> value
-            | None -> failwith "No handler found")
 
-    let terminateEvent : (Effect<unit option> -> Effect<unit>) = Effect.map ignore
 
 module WriteApplication =
     open Middlewares
---
+++ src/Application/NBB.Application.Mediator.FSharp/PipelineUtils.fs
@@ -0,0 +1,14 @@
+﻿
+namespace NBB.Application.Mediator.FSharp
+
+open NBB.Core.Effects.FSharp
+
+module PipelineUtils =
+    let terminateRequest<'a> : (Effect<'a option> -> Effect<'a>) =
+        Effect.map
+            (function
+            | Some value -> value
+            | None -> failwith "No handler found")
+
+    let terminateEvent : (Effect<unit option> -> Effect<unit>) = Effect.map ignore
+
