ff97e57f7c055db68731bf24f0cc3ad52123acbb
Radu Popovici <oncicaradupopovici@gmail.com>
2021-07-20 13:39:54 +0300

    Feature/event store effects (#165)
    * event store effects
    
    * removed sample fn
    
    Co-authored-by: Radu Popovici <rpopovici@totalsoft.ro>

---
+++ src/EventStore/NBB.EventStore.Effects/EventStore.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NBB.Core.Effects;
+using NBB.EventStore.Abstractions;
+
+namespace NBB.EventStore.Effects
+{
+    public static class AppendEventsToStream
+    {
+        public record SideEffect(string Stream, IEnumerable<object> Events, int? ExpectedVersion) : ISideEffect;
+        internal class Handler : ISideEffectHandler<SideEffect, Unit>
+        {
+            private readonly IEventStore _eventStore;
+
+            public Handler(IEventStore eventStore)
+            {
+                _eventStore = eventStore;
+            }
+
+            public async Task<Unit> Handle(SideEffect sideEffect, CancellationToken cancellationToken = default)
+            {
+                var (stream, events, expectedVersion) = sideEffect;
+                await _eventStore.AppendEventsToStreamAsync(stream, events, expectedVersion, cancellationToken);
+                return Unit.Value;
+            }
+        }
+    }
+
+    public static class GetEventsFromStream
+    {
+        public record SideEffect(string Stream, int? StartFromVersion) : ISideEffect<List<object>>;
+        internal class Handler : ISideEffectHandler<SideEffect, List<object>>
+        {
+            private readonly IEventStore _eventStore;
+
+            public Handler(IEventStore eventStore)
+            {
+                _eventStore = eventStore;
+            }
+
+            public Task<List<object>> Handle(SideEffect sideEffect, CancellationToken cancellationToken = default)
+            {
+                var (stream, startFromVersion) = sideEffect;
+                return _eventStore.GetEventsFromStreamAsync(stream, startFromVersion, cancellationToken);
+            }
+        }
+    }
+
+    public static class EventStore
+    {
+        public static Effect<Unit> AppendEventsToStream(string stream, IEnumerable<object> events, int? expectedVersion = null)
+            => Effect.Of<AppendEventsToStream.SideEffect, Unit>(new AppendEventsToStream.SideEffect(stream, events, expectedVersion));
+        public static Effect<List<object>> GetEventsFromStream(string stream, int? startFromVersion = null)
+            => Effect.Of<GetEventsFromStream.SideEffect, List<object>>(new GetEventsFromStream.SideEffect(stream, startFromVersion));
+    }
+
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddEventStoreEffects(this IServiceCollection services)
+        {
+            services.AddSingleton<ISideEffectHandler<AppendEventsToStream.SideEffect, Unit>, AppendEventsToStream.Handler>();
+            services.AddSingleton<ISideEffectHandler<GetEventsFromStream.SideEffect, List<object>>, GetEventsFromStream.Handler>();
+            return services;
+        }
+    }
+}
---
+++ src/EventStore/NBB.EventStore.Effects/Readme.md
@@ -0,0 +1,34 @@
+﻿# NBB.EventStore.Effects
+
+This package provides event-store side effects and handlers for the NBB effects infrastructure.
+
+## NuGet install
+```
+dotnet add package NBB.EventStore.Effects
+```
+
+## Registration
+You need to register the side-effects somewhere in the composition root, like so:
+
+```csharp
+services.AddEventStoreEffects();
+```
+
+## Sample usage
+```csharp
+var eff = EventStore.GetEventsFromStream("stream1");
+
+var eff2 = EventStore.AppendEventsToStream("stream2", new object[] { new SomethingHappened { Value = 1 } });
+
+var eff3 =
+    EventStore.GetEventsFromStream("stream1").Then(
+        events => EventStore.AppendEventsToStream("stream2", events));
+
+var eff4 =
+    from events in EventStore.GetEventsFromStream("stream1")
+    from _ in EventStore.AppendEventsToStream("stream2", events)
+    select _;
+```
+
+
+
