f19fc9d3b771101bf09e8834bdb2ff0c575642d8
Vasile Cuzmin <vcuzmin@totalsoft.ro>
2021-09-01 15:36:01 +0300

    reduce the SchemaMessageValidatorMiddleware only to two mandatory checks

--- src/Messaging/NBB.Messaging.Host/MessagingPipeline/SchemaMessageValidatorMiddleware.cs
+++ src/Messaging/NBB.Messaging.Host/MessagingPipeline/SchemaMessageValidatorMiddleware.cs
@@ -19,17 +19,11 @@ namespace NBB.Messaging.Host
     {
         public async Task Invoke(MessagingContext context, CancellationToken cancellationToken, Func<Task> next)
         {
-            if (!context.MessagingEnvelope.Headers.TryGetValue(MessagingHeaders.MessageType, out var _))
-                throw new Exception($"Message of type {context.MessagingEnvelope.Payload.GetType().GetPrettyName()} does not contain {MessagingHeaders.MessageType} header.");
-
             if (!context.MessagingEnvelope.Headers.TryGetValue(MessagingHeaders.CorrelationId, out var _))
                 throw new Exception($"Message of type {context.MessagingEnvelope.Payload.GetType().GetPrettyName()} does not contain {MessagingHeaders.CorrelationId} header.");
 
-            if (!context.MessagingEnvelope.Headers.TryGetValue(MessagingHeaders.MessageId, out var _))
-                throw new Exception($"Message of type {context.MessagingEnvelope.Payload.GetType().GetPrettyName()} does not contain {MessagingHeaders.MessageId} header.");
-
-            if (!context.MessagingEnvelope.Headers.TryGetValue(MessagingHeaders.PublishTime, out var _))
-                throw new Exception($"Message of type {context.MessagingEnvelope.Payload.GetType().GetPrettyName()} does not contain {MessagingHeaders.PublishTime} header.");
+            if (!context.MessagingEnvelope.Headers.TryGetValue(MessagingHeaders.Source, out var _))
+                throw new Exception($"Message of type {context.MessagingEnvelope.Payload.GetType().GetPrettyName()} does not contain {MessagingHeaders.Source} header.");
 
             await next();
         }
