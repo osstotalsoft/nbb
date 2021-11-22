75ceb1622cec20d9c12079176c181b1994566434
dsimionescu@totalsoft.ro <dsimionescu@totalsoft.ro>
2021-11-22 12:49:21 +0200

    added token resolvers

---
+++ src/MultiTenancy/NBB.MultiTenancy.Identification.Http/BearerTokenTenantIdTokenResolver.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) TotalSoft.
+// This source code is licensed under the MIT license.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using NBB.MultiTenancy.Identification.Resolvers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBB.MultiTenancy.Identification.Http
+{
+    public class BearerTokenTenantIdTokenResolver : ITenantTokenResolver
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _parameterName;
+
+        public BearerTokenTenantIdTokenResolver(IHttpContextAccessor httpContextAccessor, string parameterName)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _parameterName = parameterName;
+        }
+
+        public Task<string> GetTenantToken()
+        {
+            if (!_httpContextAccessor.HttpContext.Request.Headers.ContainsKey(HeaderNames.Authorization))
+            {
+                return Task.FromResult(string.Empty);
+            }
+            var tokenString = _httpContextAccessor
+                .HttpContext
+                .Request
+                .Headers[HeaderNames.Authorization]
+                .ToString()
+                .Replace("Bearer ", "");
+
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(tokenString);
+            if (jsonToken is not JwtSecurityToken token)
+            {
+                return default;
+            }
+            var claim = token.Claims.FirstOrDefault(x => x.Type == _parameterName);
+            return Task.FromResult(claim?.Value);
+        }
+    }
+}
---
+++ src/MultiTenancy/NBB.MultiTenancy.Identification.Http/ReferrerTenantIdTokenResolver.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) TotalSoft.
+// This source code is licensed under the MIT license.
+
+using Microsoft.AspNetCore.Http;
+using NBB.MultiTenancy.Identification.Resolvers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NBB.MultiTenancy.Identification.Http
+{
+    /// <summary>
+    /// services.AddTenantTokenResolver<TenantIdRefererHttpTokenResolver>("nbb-tenantId=([({]?[a-fA-F0-9]{8}[-]?([a-fA-F0-9]{4}[-]?){3}[a-fA-F0-9]{12}[})]?)")
+    /// </summary>
+    public class ReferrerTenantIdTokenResolver : ITenantTokenResolver
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private const string HeaderNameDefault = "Referer";
+        private readonly string _regEx;
+
+        public ReferrerTenantIdTokenResolver(string regEx, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _regEx = regEx;
+        }
+
+        public Task<string> GetTenantToken()
+        {
+            if (_httpContextAccessor?.HttpContext?.Request == null)
+            {
+                return Task.FromResult((string)null);
+            }
+            if (_httpContextAccessor.HttpContext.Request.Headers.TryGetValue(HeaderNameDefault, out var value))
+            {
+                var url = value.ToString();
+                var match = Regex.Match(url, _regEx, RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    var tenantId = match.Groups[1].Value;
+                    return Task.FromResult(tenantId);
+                }
+            }
+
+            return Task.FromResult((string)null);
+        }
+    }
+}
