69d4b53254b3d195752972e46ad201da155e483f
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2022-01-05 15:34:06 +0200

    EF upgrade

--- src/Data/NBB.Data.EntityFramework.MultiTenancy/EntityTypeExtensions.cs
+++ src/Data/NBB.Data.EntityFramework.MultiTenancy/EntityTypeExtensions.cs
@@ -9,5 +9,8 @@ namespace NBB.Data.EntityFramework.MultiTenancy
     {
         public static bool IsMultiTenant(this IEntityType entityType) 
             => (bool?)entityType.FindAnnotation(MultiTenancy.MultiTenantAnnotation)?.Value ?? false;
+
+        public static bool IsMultiTenant(this IMutableEntityType entityType)
+            => IsMultiTenant(entityType as IEntityType);
     }
 }
