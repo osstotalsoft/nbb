c7af16cd9137826afac2cab0775a297a2b9f927f
lucian <lghinet@totalsoft.ro>
2022-02-08 19:45:21 +0200

    refactor configuration (#205)
    change configuration source, to support both single string and complex object connectionStrings

--- src/MultiTenancy/NBB.MultiTenancy.Abstractions/Configuration/TenantConfigurationExtensions.cs
+++ src/MultiTenancy/NBB.MultiTenancy.Abstractions/Configuration/TenantConfigurationExtensions.cs
@@ -7,13 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NBB.MultiTenancy.Abstractions.Configuration
+namespace NBB.MultiTenancy.Abstractions.Configuration;
+
+public static class TenantConfigurationExtensions
 {
-    public static class TenantConfigurationExtensions
+    public static string GetConnectionString(this ITenantConfiguration config, string name)
     {
-        public static string GetConnectionString(this ITenantConfiguration config, string name)
+        var splitted = config.GetValue<ConnectionStringDetails>($"ConnectionStrings:{name}");
+        if (splitted != null)
         {
-            return config.GetValue<string>($"ConnectionStrings:{name}");
+            return
+                $"Server={splitted.Server};Database={splitted.Database};User Id={splitted.UserName};Password={splitted.Password};{splitted.OtherParams}";
         }
+
+        return config.GetValue<string>($"ConnectionStrings:{name}");
+    }
+
+    private class ConnectionStringDetails
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string OtherParams { get; set; }
     }
 }
