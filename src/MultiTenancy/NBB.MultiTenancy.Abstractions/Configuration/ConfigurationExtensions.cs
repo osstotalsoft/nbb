56a214d9dc0e36d2cf71abd9a9591ecff8cc6d85
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2022-03-21 15:12:36 +0200

    Added configuration extension to check for multi-tenancy (#217)

---
+++ src/MultiTenancy/NBB.MultiTenancy.Abstractions/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) TotalSoft.
+// This source code is licensed under the MIT license.
+
+using NBB.MultiTenancy.Abstractions.Options;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Configuration
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static bool IsMultiTenant(this IConfiguration configuration)
+        {
+            return nameof(TenancyType.MultiTenant).Equals(configuration["MultiTenancy:TenancyType"], System.StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
