aa35803e4f558563e3b3eddb2a407b9b8d593e91
Radu Popovici <oncicaradupopovici@gmail.com>
2022-11-17 18:13:17 +0200

    UsingInvoiceLock

--- samples/MicroServices/NBB.Invoices/NBB.Invoices.Application/CommandHandlers/ProcessInvoiceCommandHandler.cs
+++ samples/MicroServices/NBB.Invoices/NBB.Invoices.Application/CommandHandlers/ProcessInvoiceCommandHandler.cs
@@ -5,7 +5,7 @@ using MediatR;
 using NBB.Data.Abstractions;
 using NBB.Invoices.Domain.InvoiceAggregate;
 using NBB.Invoices.PublishedLanguage;
-using System.Reflection.Metadata;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,37 +22,33 @@ namespace NBB.Invoices.Application.CommandHandlers
             this._invoiceLockRepository = invoiceLockRepository;
         }
 
-        public async Task<Unit> Handle(ProcessInvoice command, CancellationToken cancellationToken)
+        public Task<Unit> Handle(ProcessInvoice command, CancellationToken cancellationToken)
+            => UsingInvoiceLock(command.InvoiceId, lockTimeoutMs: 10000, async () =>
+            {
+                var invoice = await _invocieRepository.GetByIdAsync(command.InvoiceId, cancellationToken);
+                invoice.Process(); //takes 1000 ms cpu time
+                await _invocieRepository.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            });
+
+        private async Task<T> UsingInvoiceLock<T>(Guid invoiceId, int lockTimeoutMs, Func<Task<T>> func)
         {
-            var invoiceLock = await _invoiceLockRepository.GetByIdAsync(command.InvoiceId) ??
-                new InvoiceLock(command.InvoiceId, lockTimeoutMs: 10000);
+            var invoiceLock = await _invoiceLockRepository.GetByIdAsync(invoiceId) ??
+                new InvoiceLock(invoiceId, lockTimeoutMs);
 
             invoiceLock.Acquire(); //throws InvoiceLockAlreadyAcquiredException if locked
-
             await _invoiceLockRepository.SaveAsync(invoiceLock); //throws ConcurencyException => retry forever
-            //aici sunt protejat
+
             try
             {
-                await InternalHandle(command, cancellationToken);
+                return await func();
             }
             finally
             {
                 invoiceLock.Release();
                 await _invoiceLockRepository.SaveAsync(invoiceLock);
             }
-
-            return Unit.Value;
-        }
-
-        private async Task<Unit> InternalHandle(ProcessInvoice command, CancellationToken cancellationToken)
-        {
-            var invoice = await _invocieRepository.GetByIdAsync(command.InvoiceId, cancellationToken);
-            invoice.Process(); //takes 1000 ms cpu time
-            await _invocieRepository.SaveChangesAsync(cancellationToken);
-
-            return Unit.Value;
         }
     }
-
-    
 }
