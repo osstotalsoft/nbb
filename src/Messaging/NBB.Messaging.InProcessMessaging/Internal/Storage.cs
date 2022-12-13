f0c8b462c09a889e4a0878c6906837ef8e2121b8
Radu Popovici <oncicaradupopovici@gmail.com>
2022-12-13 21:47:56 +0200

    dipose subscription

--- src/Messaging/NBB.Messaging.InProcessMessaging/Internal/Storage.cs
+++ src/Messaging/NBB.Messaging.InProcessMessaging/Internal/Storage.cs
@@ -11,6 +11,21 @@ namespace NBB.Messaging.InProcessMessaging.Internal
 {
     public class Storage : IStorage
     {
+        internal class DelegateDisposable : IDisposable
+        {
+            private readonly Action _onDispose;
+
+            public DelegateDisposable(Action onDispose)
+            {
+                _onDispose = onDispose;
+            }
+
+            public void Dispose()
+            {
+                _onDispose();
+            }
+        }
+
         private readonly ConcurrentDictionary<string, ConcurrentQueue<byte[]>> _queues = new();
         private readonly HashSet<string> _subscriptions = new();
         private readonly ConcurrentDictionary<string, AutoResetEvent> _brokersAutoReset = new();
@@ -29,7 +44,7 @@ namespace NBB.Messaging.InProcessMessaging.Internal
             }
         }
 
-        public async Task AddSubscription(string topic, Func<byte[], Task> handler,
+        public async Task<IDisposable> AddSubscription(string topic, Func<byte[], Task> handler,
             CancellationToken cancellationToken = default)
         {
             lock (_subscriptions)
@@ -42,6 +57,7 @@ namespace NBB.Messaging.InProcessMessaging.Internal
 
             await Task.Yield();
             var _ = Task.Run(async () => { await StartBroker(topic, handler, cancellationToken); }, cancellationToken);
+            return new DelegateDisposable(() => { _subscriptions.Remove(topic); });
         }
 
         private async Task StartBroker(string topic, Func<byte[], Task> handler,
---
+++ test/UnitTests/Messaging/NBB.Messaging.InProcessMessaging.Tests/InProcessMessagingTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) TotalSoft.
+// This source code is licensed under the MIT license.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NBB.Messaging.Abstractions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NBB.Messaging.InProcessMessaging.Tests
+{
+    public class InProcessMessagingTests
+    {
+        [Fact]
+        public async Task subsequent_msg_bus_subscriptions_for_same_topic()
+        {
+            //Arrange
+            using var sp = BuildServiceProvider();
+            var msgBus = sp.GetRequiredService<IMessageBus>();
+
+            Func<Task> subscribe = async () =>
+            {
+                using var sub = await msgBus.SubscribeAsync(e => Task.CompletedTask, options: MessagingSubscriberOptions.Default with { TopicName = "SomeTopic" });
+            };
+
+            //Act
+            await subscribe();
+            await subscribe(); //should not throw already subscribed
+
+        }
+
+        private ServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+            services.AddMessageBus().AddInProcessTransport();
+
+            return services.BuildServiceProvider();
+
+        }
+    }
+}
