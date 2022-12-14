8fe2bbdf66aeac4598d663750cc06d6edd3f628b
Radu Popovici <oncicaradupopovici@gmail.com>
2022-12-14 09:58:13 +0200

    InProcessMessaging - fix unsubscribe (#248)
    * dipose subscription
    
    * update Build.ps1
    
    * unsubscribe_should_stop_broker_task

--- src/Messaging/NBB.Messaging.InProcessMessaging/Internal/IStorage.cs
+++ src/Messaging/NBB.Messaging.InProcessMessaging/Internal/IStorage.cs
@@ -10,6 +10,6 @@ namespace NBB.Messaging.InProcessMessaging.Internal
     public interface IStorage
     {
         void Enqueue(byte[] msg, string topic);
-        Task AddSubscription(string topic, Func<byte[], Task> handler, CancellationToken cancellationToken = default);
+        Task<IDisposable> AddSubscription(string topic, Func<byte[], Task> handler, CancellationToken cancellationToken = default);
     }
 }
---
+++ test/UnitTests/Messaging/NBB.Messaging.InProcessMessaging.Tests/InProcessMessagingTests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) TotalSoft.
+// This source code is licensed under the MIT license.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NBB.Messaging.Abstractions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NBB.Messaging.InProcessMessaging.Tests
+{
+    public class InProcessMessagingTests
+    {
+        [Fact]
+        public async Task subsequent_msg_bus_subscriptions_for_same_topic()
+        {
+            //Arrange
+            using var sp = BuildServiceProvider();
+            var msgBus = sp.GetRequiredService<IMessageBus>();
+
+            Func<Task> subscribe = async () =>
+            {
+                using var sub = await msgBus.SubscribeAsync(e => Task.CompletedTask, options: MessagingSubscriberOptions.Default with { TopicName = "SomeTopic" });
+            };
+
+            //Act
+            await subscribe();
+            await subscribe(); //should not throw already subscribed
+
+        }
+
+        //[Fact]
+        //public async Task unsubscribe_should_stop_broker_task()
+        //{
+        //    //Arrange
+        //    using var sp = BuildServiceProvider();
+        //    var msgBus = sp.GetRequiredService<IMessageBus>();
+
+        //    //Act
+        //    var sub = await msgBus.SubscribeAsync(e => Task.CompletedTask, options: MessagingSubscriberOptions.Default with { TopicName = "SomeTopic" });
+        //    sub.Dispose();
+        //    await Task.Delay(10000);
+
+        //    //Assert
+
+
+        //}
+
+        private ServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+            services.AddMessageBus().AddInProcessTransport();
+
+            return services.BuildServiceProvider();
+
+        }
+    }
+}
