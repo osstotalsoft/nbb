52c1f525ec5173447744d8074c9354147122dc37
Dorin Simionescu <dsimionescu@totalsoft.ro>
2023-02-14 14:54:19 +0200

    fix IsMultiTenant

--- src/Data/NBB.Data.EntityFramework.MultiTenancy/EntityTypeExtensions.cs
+++ src/Data/NBB.Data.EntityFramework.MultiTenancy/EntityTypeExtensions.cs
@@ -7,8 +7,18 @@ namespace NBB.Data.EntityFramework.MultiTenancy
 {
     public static class EntityTypeExtensions
     {
-        public static bool IsMultiTenant(this IEntityType entityType) 
-            => (bool?)entityType.FindAnnotation(MultiTenancy.MultiTenantAnnotation)?.Value ?? false;
+        public static bool IsMultiTenant(this IEntityType entityType)
+        {
+            while (entityType != null)
+            {
+                var hasMultiTenantAnnotation = (bool?)entityType.FindAnnotation(MultiTenancy.MultiTenantAnnotation)?.Value ?? false;
+                if (hasMultiTenantAnnotation)
+                    return true;
+                entityType = entityType.BaseType;
+            }
+
+            return false;
+        }
 
         public static bool IsMultiTenant(this IMutableEntityType entityType)
             => IsMultiTenant(entityType as IEntityType);
