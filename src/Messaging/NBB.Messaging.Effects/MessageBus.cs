b280503c943ac300a94ab26574e73695ecfe675b
Radu Popovici <oncicaradupopovici@gmail.com>
2023-10-03 12:14:29 +0300

    MessageBus pusblish effect 2 (#263)

--- src/Messaging/NBB.Messaging.Effects/MessageBus.cs
+++ src/Messaging/NBB.Messaging.Effects/MessageBus.cs
@@ -2,11 +2,12 @@
 // This source code is licensed under the MIT license.
 
 using NBB.Core.Effects;
+using NBB.Messaging.Abstractions;
 
 namespace NBB.Messaging.Effects
 {
     public static class MessageBus
     {
-        public static Effect<Unit> Publish(object message) => Effect.Of<PublishMessage.SideEffect, Unit>(new PublishMessage.SideEffect(message));
+        public static Effect<Unit> Publish(object message, MessagingPublisherOptions options = null) => Effect.Of<PublishMessage.SideEffect, Unit>(new PublishMessage.SideEffect(message, options));
     }
 }
