eea2f84e597e4c69092d754b07995ca6ea077be3
Dorin Simionescu <dsimionescu@totalsoft.ro>
2024-04-02 15:46:00 +0300

    TenantContext Properties

--- src/MultiTenancy/NBB.MultiTenancy.Abstractions/Context/TenantContext.cs
+++ src/MultiTenancy/NBB.MultiTenancy.Abstractions/Context/TenantContext.cs
@@ -1,20 +1,28 @@
 ﻿// Copyright (c) TotalSoft.
 // This source code is licensed under the MIT license.
 
+using System.Collections.Generic;
+
 namespace NBB.MultiTenancy.Abstractions.Context
 {
     public class TenantContext
     {
         public Tenant Tenant { get; }
-
+        public Dictionary<string, object> Properties { get; set; } = [];
         public TenantContext(Tenant tenant)
         {
             Tenant = tenant;
         }
 
+        public TenantContext(Tenant tenant, Dictionary<string, object> properties)
+        {
+            Tenant = tenant;
+            Properties = properties;
+        }
+
         public TenantContext Clone()
         {
-            return new TenantContext(new Tenant(Tenant.TenantId, Tenant.Code));
+            return new TenantContext(new Tenant(Tenant.TenantId, Tenant.Code), new Dictionary<string, object>(Properties));
         }
     }
 }
